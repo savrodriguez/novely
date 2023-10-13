ecf11e8208b7e92487ea734f969e57849eb746e5
Savana <rodriguezsavana543@gmail.com>
2023-10-12 19:07:00 -0700

    login and sign up form created

---
+++ scripts.js
@@ -0,0 +1,39 @@
+//LOGIN / SIGNUP FORM
+function setFromMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`)
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector("form__input-error-message").textContent = message;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#form");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+
+    });   
+        document.querySelector("#linkLogin").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.remove("form--hidden");
+            createAccountForm.classList.add("form--hidden");
+
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+
+        //preform login
+        setFromMessage(loginForm, "error", "Invalid Response :( ");
+    });
+});
